bffaefa5f08f1d0f31c788f19066dd7cfaef53fd
blackberrypineapple <blackberrypineapple@protonmail.com>
2025-11-20 13:36:19 +0000

    try fix light toasts in dark mode

--- app/src/main/java/com/drdisagree/iconify/utils/overlay/compiler/OnDemandCompiler.kt
+++ app/src/main/java/com/drdisagree/iconify/utils/overlay/compiler/OnDemandCompiler.kt
@@ -175,7 +175,7 @@ object OnDemandCompiler {
         if (mOverlayName != "TSTFRM" || !requiresNewToastStyle()) return
 
         Shell.cmd(
-            "find \"$TEMP_CACHE_DIR/$mPackage/$mOverlayName/\" -type f -name \"*.xml\" -exec sh -c 'for file; do if echo \"\$file\" | grep -q \"/[^/]*-night/\"; then sed -i \"s/?android:colorBackgroundFloating/@*android:color\\/system_neutral2_800/g\" \"\$file\"; else sed -i \"s/?android:colorBackgroundFloating/@*android:color\\/system_neutral2_10/g\" \"\$file\"; fi; done' sh {} +"
+            "find \"$TEMP_CACHE_DIR/$mPackage/$mOverlayName/\" -type f -name \"*.xml\" -exec sh -c 'for file; do if echo \"\$file\" | grep -q \"/[^/]*-night/\"; then sed -i \"s/?android:colorBackgroundFloating/@*android:color\\/system_neutral2_800/g\" \"\$file\"; else sed -i \"s/?android:colorBackgroundFloating/@*android:color\\/system_neutral2_800/g\" \"\$file\"; fi; done' sh {} +"
         ).exec()
     }
 }
